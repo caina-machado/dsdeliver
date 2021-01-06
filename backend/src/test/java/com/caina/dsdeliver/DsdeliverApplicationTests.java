edffd0fd26079645a66e32599579646abad1aadf
Cainã Machado <cwaxtd@gmail.com>
2021-01-06 16:20:26 -0300

    Project created

---
+++ backend/src/test/java/com/caina/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caina.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
