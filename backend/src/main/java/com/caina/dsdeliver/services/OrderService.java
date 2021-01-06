9040aa3c6015bf8c3d4a9b995c7539478ca1d6c0
Cainã Machado <cwaxtd@gmail.com>
2021-01-06 17:55:31 -0300

    POST /orders

--- backend/src/main/java/com/caina/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/caina/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.caina.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.caina.dsdeliver.dtos.OrderDto;
+import com.caina.dsdeliver.dtos.ProductDto;
 import com.caina.dsdeliver.entities.Order;
+import com.caina.dsdeliver.entities.Product;
+import com.caina.dsdeliver.entities.enums.OrderStatus;
 import com.caina.dsdeliver.repositories.OrderRepository;
+import com.caina.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,26 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDto> findAll() {
 		List<Order> list = orderRepository.findPendingOrdersWithProductsByMoment();
 		return list.stream().map(x -> new OrderDto(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDto insert(OrderDto dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		
+		for(ProductDto p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			
+			order.getProducts().add(product);
+		}
+		
+		order = orderRepository.save(order);
+		return new OrderDto(order);
 	}
 }
