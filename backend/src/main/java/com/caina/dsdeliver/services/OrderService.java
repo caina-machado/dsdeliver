1dd0ede75b0434cdb52110f9fcb290417db39c23
Cainã Machado <cwaxtd@gmail.com>
2021-01-06 18:01:06 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/caina/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/caina/dsdeliver/services/OrderService.java
@@ -44,4 +44,14 @@ public class OrderService {
 		order = orderRepository.save(order);
 		return new OrderDto(order);
 	}
+	
+	@Transactional
+	public OrderDto setDelivered(Long id) {
+		Order order = orderRepository.getOne(id);
+		
+		order.setStatus(OrderStatus.DELIVERED);
+		
+		order = orderRepository.save(order);
+		return new OrderDto(order);
+	}
 }
