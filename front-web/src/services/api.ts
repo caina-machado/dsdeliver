6e8bde22041da185dad8137689e44620b790d17c
Cainã Machado <cwaxtd@gmail.com>
2021-01-09 18:09:05 -0300

    Add env vars

--- front-web/src/services/api.ts
+++ front-web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "../pages/Orders/types";
 
-const BASE_URL = "https://caina-dsdeliver.herokuapp.com";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
